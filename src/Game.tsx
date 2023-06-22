de4257b01a1fcc1bc13f73a6cee6f59e67b2e709
Viktor Pavlovic <viktorpavlovic117@gmail.com>
2023-06-22 14:56:53 +0200

    adding game logic and scores

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,17 +1,93 @@
 import { useState, useEffect } from "react";
 import Square from "./Square";
 
-const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
+type Scores = {
+  [key: string]: number;
+};
 
+const INITIAL_GAME_STATE = ["", "", "", "", "", "", "", "", ""];
+const INITIAL_SCORES: Scores = { X: 0, O: 0 };
+const WINNING_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 function Game() {
   const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
   const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [scores, setScores] = useState(INITIAL_SCORES);
+
+  useEffect(() => {
+    const storedScores = localStorage.getItem("scores");
+    if (storedScores) {
+      setScores(JSON.parse(storedScores));
+    }
+  }, []);
+
   const changePlayer = () => {
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   };
   useEffect(() => {
-    changePlayer();
+    if(gameState===INITIAL_GAME_STATE){
+      return
+    }
+    checkForWinner();
   }, [gameState]);
+
+  const resetBoard = () => {
+    setGameState(INITIAL_GAME_STATE);
+  };
+
+  const handleWin = () => {
+    window.alert(`Congrats player ${currentPlayer}: you are the winner`);
+    const newPlayerScore = scores[currentPlayer] + 1;
+    const newScores = { ...scores };
+    newScores[currentPlayer] = newPlayerScore;
+    setScores(newScores);
+    localStorage.setItem("scores", JSON.stringify(newScores));
+    resetBoard();
+  };
+  const handleDraw = () => {
+    window.alert("Game ended in a draw");
+    resetBoard();
+  };
+
+  const checkForWinner = () => {
+    let roundWon = false;
+    for (let i = 0; i < WINNING_COMBOS.length; i++) {
+      const winCombo = WINNING_COMBOS[i];
+      let a = gameState[winCombo[0]];
+      let b = gameState[winCombo[1]];
+      let c = gameState[winCombo[2]];
+
+      if ([a, b, c].includes("")) {
+        continue;
+      }
+      if (a === b && b === c) {
+        roundWon = true;
+        break;
+      }
+    }
+    if (roundWon) {
+      setTimeout(() => {
+        handleWin();
+      }, 500);
+
+      return;
+    }
+    if (!gameState.includes("")) {
+      setTimeout(() => {
+        handleDraw();
+      }, 500);
+    }
+    changePlayer();
+  };
+
   const handleCellClick = (event: any) => {
     const cellIndex = Number(event.target.getAttribute("data-cell-index"));
     const currentValue = gameState[cellIndex];
@@ -37,7 +113,17 @@ function Game() {
             />
           ))}
         </div>
-        <div>Scores goes here</div>
+        <div className="mx-auto w-96 text-2xl text-serif">
+          <p className="text-white mt-5 ">
+            Next Player <span>{currentPlayer}</span>
+          </p>
+          <p className="text-white mt-5 ">
+            Player X wins: <span>{scores["X"]}</span>
+          </p>
+          <p className="text-white mt-5 ">
+            Player O wins: <span>{scores["O"]}</span>
+          </p>
+        </div>
       </div>
     </div>
   );
